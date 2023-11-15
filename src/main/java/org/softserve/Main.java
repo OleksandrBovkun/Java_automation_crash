2b391ab0dc49bbe6b76435ff6822bd5583d6fa3d
0xKolyanus <mukola.1zt.@gmail.com>
2023-11-15 19:40:43 +0200

    added clean project

---
+++ src/main/java/org/softserve/Main.java
@@ -0,0 +1,10 @@
+package org.softserve;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static int adder(int a, int b){
+        return a+b;
+    }
+}
