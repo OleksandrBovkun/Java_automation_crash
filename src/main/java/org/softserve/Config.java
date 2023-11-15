eef163a6abc36f0819685cc67c5cac27dcc5f4f3
0xKolyanus <mukola.1zt.@gmail.com>
2023-11-15 20:23:57 +0200

    added a new config for multi browser

---
+++ src/main/java/org/softserve/Config.java
@@ -0,0 +1,5 @@
+package org.softserve;
+
+public class Config {
+    public static final String BROWSER = "";
+}
---
+++ src/main/java/org/softserve/SetupDriver.java
@@ -0,0 +1,37 @@
+package org.softserve;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.time.Duration;
+
+import static org.softserve.Config.BROWSER;
+
+public class SetupDriver {
+    public static WebDriver createDriver(){
+        WebDriver driver = null;
+        switch (BROWSER){
+            case "Edge":
+//                WebDriverManager.chromedriver().setup();
+//                ChromeOptions options = new ChromeOptions();
+//                options.addArguments("--remote-allow-origins=*");
+//                driver = new ChromeDriver(options);
+                driver = new EdgeDriver();
+                break;
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "FireFox":
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+//                Assert.fail("Incorrect platform, or browser name : " + BROWSER);
+        }
+        driver.manage().window().maximize();
+//        driver.manage().timeouts().
+//                implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT));
+        return driver;
+    }
+}
