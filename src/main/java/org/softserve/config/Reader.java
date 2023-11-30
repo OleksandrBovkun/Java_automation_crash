4b4f245b9bd7dc3b0f5d2691ffd26bd5bafdf012
Iwba <ivankoprogamer@gmail.com>
2023-11-30 15:47:56 +0200

    added Reader loger

--- src/main/java/org/softserve/config/Reader.java
+++ src/main/java/org/softserve/config/Reader.java
@@ -3,6 +3,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Reader {
 
@@ -10,11 +12,12 @@ public class Reader {
     private Reader(){}
     public static Config getConfig(){
         ObjectMapper mapper = new ObjectMapper();
+        final  Logger logger = Logger.getLogger("Reader LOGGER");
         if (config == null) {
             try {
                 config = mapper.readValue(new File("src/main/resources/config.json"), Config.class);
             }catch (IOException ioException){
-
+                logger.log(Level.WARNING, ioException.getMessage());
             }
         }
         return config;
